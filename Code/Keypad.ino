44c8fc5d5829c4e6888673d1484e2e87a69ae8f0
up201805142 <up201805142@g.uporto.pt>
2023-05-17 10:35:11 +0100

    Beautify + keypad code

---
+++ Code/Keypad.ino
@@ -0,0 +1,84 @@
+#include <Keypad.h>
+#define MAX_SIZE 256
+//#define DEBUG
+
+const byte ROWS = 4; 
+const byte COLS = 4; 
+
+char hexaKeys[ROWS][COLS] = {
+  {'1', '2', '3','A'},
+  {'4', '5', '6','B'},
+  {'7', '8', '9','C'},
+  {'*', '0', '#','D'}
+};
+
+char curChar;
+unsigned long curTime;
+char prevChar = 'x';
+unsigned long prevTime;
+int count = 0;
+int maxCnt[] = {1,3,3,3,3,3,3,4,3,4};
+char dispChar[10][5] = {" ", "/.@", "ABC", "DEF", "GHI", "JKL", "MNO", "PQRS",
+                "TUV", "WXYZ"};
+
+byte rowPins[ROWS] = {2,3,4,5}; 
+byte colPins[COLS] = {6,7,8,9}; 
+
+char message[MAX_SIZE];
+
+Keypad customKeypad = Keypad(makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+#ifdef DEBUG
+curChar = customKeypad.getKey();
+Serial.print(curChar);
+
+#endif
+
+  #ifndef DEBUG
+  bool read = false;
+  char c;
+  curChar = customKeypad.getKey();
+  read = (curChar == NO_KEY ? false : true);
+
+  curTime = millis();
+  int pivot = prevChar - '0';
+  if(read) // Lido
+  {
+    if(curChar == prevChar) {
+      count++;
+    } 
+    else if (count == 0){
+      count++;
+      prevChar = curChar;
+      prevTime = curTime;
+    }
+    else {
+      pivot = prevChar - '0';
+      c = dispChar[pivot][(count-1) % maxCnt[pivot]];
+      Serial.print(c);
+      strncat(message, c, MAX_SIZE);
+      count = 1;
+      prevChar = curChar;
+      prevTime = curTime;
+    }
+  } 
+  else // Nao lido
+  {
+    if(curTime - prevTime <= 1000) return;
+    if(count == 0) {
+      prevTime = curTime; 
+      return; 
+    }
+    c = dispChar[pivot][(count-1) % maxCnt[pivot]];
+    Serial.print(c);
+    strncat(message, c, MAX_SIZE);
+    count = 0;
+    prevChar = 'x';
+    prevTime = curTime;
+  }
+#endif
+}
